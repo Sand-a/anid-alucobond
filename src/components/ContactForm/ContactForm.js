e46dc4faf8c6b673b3ffcdad31f9b30a4fabd4d2
Sand-a <s_anid@hotmail.com>
2023-04-03 09:00:36 +0100

    no reveal in contact

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,8 +1,6 @@
 import React, { useRef } from "react";
-import { Link } from "react-router-dom";
+
 import "./ContactForm.css";
-import Fade from "react-reveal/Fade";
-import Flip from "react-reveal/Flip";
 
 const ContactForm = () => {
   const form = useRef();
@@ -18,9 +16,7 @@ const ContactForm = () => {
               className="contact-link"
               href="https://www.google.co.uk/maps/place/ALUCOBOND-ANID/@33.8618125,35.2896406,11z/data=!4m10!1m2!2m1!1sanid+construction!3m6!1s0x151f17c4e84d1bc1:0x87a93d4ed5f68907!8m2!3d33.8618125!4d35.5533125!15sChFhbmlkIGNvbnN0cnVjdGlvbloTIhFhbmlkIGNvbnN0cnVjdGlvbpIBCmNvbnRyYWN0b3LgAQA!16s%2Fg%2F11c51z3_3l"
             >
-              <Flip left>
-                <ion-icon name="location-outline"></ion-icon>
-              </Flip>
+              <ion-icon name="location-outline"></ion-icon>
             </a>
 
             <div className="contact-item">
@@ -31,18 +27,14 @@ const ContactForm = () => {
 
           <li className="contact-items">
             <a className="contact-link" href="tel:+9613299005">
-              <Flip left>
-                <ion-icon name="call-outline"></ion-icon>
-              </Flip>
+              <ion-icon name="call-outline"></ion-icon>
             </a>
 
             <p className="contact-item">+961 (1) 684 111</p>
           </li>
           <li className="contact-items">
             <a className="contact-link" href="tel:+9613299005">
-              <Flip left>
-                <ion-icon name="phone-portrait-outline"></ion-icon>
-              </Flip>
+              <ion-icon name="phone-portrait-outline"></ion-icon>
             </a>
             <p className="contact-item">+961 (3) 299 005</p>
           </li>
@@ -51,9 +43,7 @@ const ContactForm = () => {
               href="mailto:contact@anidconstruction.com"
               className="contact-link"
             >
-              <Flip left>
-                <ion-icon name="mail-outline"></ion-icon>
-              </Flip>
+              <ion-icon name="mail-outline"></ion-icon>
             </a>
             <p className="contact-item">contact@anidconstruction.com</p>
           </li>
@@ -63,43 +53,40 @@ const ContactForm = () => {
               href="https://www.instagram.com/alucobondlebanon/?igshid=YmMyMTA2M2Y%3D"
               className="contact-link"
             >
-              <Flip left>
-                <ion-icon name="logo-instagram"></ion-icon>
-              </Flip>
+              <ion-icon name="logo-instagram"></ion-icon>
             </a>
             <p className="contact-item">alucobondlebanon</p>
           </li>
         </ul>
-        <Fade left>
-          <form ref={form} onSubmit={sendEmail} className="form-container">
-            <h1> Send us your requirement:</h1>
-            <input
-              className="searchfield"
-              type="text"
-              placeholder="Full Name"
-              name="user_name"
-              required
-            />
-            <input
-              className="searchfield"
-              type="email"
-              placeholder="Email"
-              name="user_email"
-              required
-            />
-            <input
-              className="searchfield"
-              type="text"
-              placeholder="Subject"
-              name="subject"
-              required
-            />
-            <textarea name="message" id="" cols="30" rows="10"></textarea>
-            <button type="submit" className="btn btn-form">
-              Send Message
-            </button>
-          </form>
-        </Fade>
+
+        <form ref={form} onSubmit={sendEmail} className="form-container">
+          <h1> Send us your requirement:</h1>
+          <input
+            className="searchfield"
+            type="text"
+            placeholder="Full Name"
+            name="user_name"
+            required
+          />
+          <input
+            className="searchfield"
+            type="email"
+            placeholder="Email"
+            name="user_email"
+            required
+          />
+          <input
+            className="searchfield"
+            type="text"
+            placeholder="Subject"
+            name="subject"
+            required
+          />
+          <textarea name="message" id="" cols="30" rows="10"></textarea>
+          <button type="submit" className="btn btn-form">
+            Send Message
+          </button>
+        </form>
       </div>
     </>
   );
